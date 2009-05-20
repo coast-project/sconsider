a5fee5da6aa39d30143e495b5b0acc5ecb382bfb
Marcel Huber <m1huber@hsr.ch>
2009-05-20 08:18:13 +0200

    python script to change old to new library names in Anythings

---
+++ ChangeDLLNames.py
@@ -0,0 +1,68 @@
+import os, pdb, re
+
+excludelist = ['build', 'CVS', 'data', 'xml', 'doc', 'bin', 'lib', '.git', '.gitmodules']
+list2 = []
+excludelist.extend(list2)
+print excludelist
+libdict = {'renderers':'CoastRenderers',
+           'renderer':'CoastRenderers',
+           'actions':'CoastActions',
+           'stddataaccess':'CoastStdDataAccess',
+           'security':'CoastSecurity',
+           'SSL':'CoastSSL',
+           'ldapdataaccess':'CoastLDAPDataAccess',
+           'ldapdataaccess2':'CoastLDAP',
+           'dataaccess':'CoastDataAccess',
+           'perftest':'CoastPerfTest',
+           'applog':'CoastAppLog',
+           'TKFFunctionalRenderers':'CoastFunctionalRenderers',
+           'perftesttest':'PerfTestTest',
+           'radiusdataaccess':'CoastRadiusDataAccess',
+           'workerpoolmanagermodule':'CoastWorkerPoolManager'}
+for dirpath, dirnames, filenames in os.walk('.'):
+    dirnames[:] = [d for d in dirnames if not d in excludelist]
+    reDLL = re.compile(r"[\s\S]+/DLL\s*{([^}]+)}[\s\S]+", re.M)
+    reAny = re.compile('^.*.any$')
+    for name in filenames:
+        if reAny.match(name):
+            fname = os.path.join(dirpath, name)
+            try:
+                fo = open(fname)
+                if fo:
+                    fstr = fo.read()
+                    fo.close()
+                    mo = reDLL.match(fstr)
+                    if mo:
+                        print "file:", fname
+                        outstr = mo.string[:mo.start(1)]
+                        strGroup = mo.group(1)
+                        strout = ''
+                        for it in re.finditer(r"^([^#\S]+)(\"?(lib)?(\w+)(\.so)?\"?)([\s]*)$", strGroup, re.M):
+                            if len(strout):
+                                strout += '\n'
+                            g = it.groups()
+                            lname = g[3]
+                            if lname in libdict:
+                                kval = libdict.get(lname)
+                                strout += it.string[it.start(0):it.start(2)]
+                                strout += kval
+                                strout += it.string[it.end(2):it.end(0)]
+                            elif lname in libdict.itervalues():
+                                strout += it.string[it.start(0):it.end(0)]
+                            else:
+                                print lname, "MISSING"
+                                strout += it.string[it.start(0):it.end(0)]
+                        if len(strout):
+                            outstr += strout
+                        else:
+                            outstr += strGroup
+                        outstr += mo.string[mo.end(1):]
+                        if fstr != outstr:
+                            try:
+                                of = open(fname, 'w+')
+                                of.write(outstr)
+                                of.close()
+                            except:
+                                pass
+            except IOError:
+                pass
