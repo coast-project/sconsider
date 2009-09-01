2568ec2d6cfd12636ff705588c6067b0f24d753c
Dominik Wild <dwild@hsr.ch>
2009-09-01 14:11:56 +0200

    refactored some parts of TestfwTransformer for easier tests

--- site_tools/TestfwTransformer.py
+++ site_tools/TestfwTransformer.py
@@ -1,9 +1,5 @@
 from __future__ import with_statement
 import os, pdb, subprocess, optparse, re, socket, time, math
-import SCons.Action, SCons.Builder, SCons.Script
-from SCons.Script import AddOption, GetOption
-import StanfordUtils
-import RunBuilder
 from xmlbuilder import XMLBuilder
 
 class Result(object):
@@ -71,101 +67,117 @@ class State(object):
         return method_missing
 
 class StartedState(State):
-    def end(self, tests, assertions, msecs):
+    def end(self, tests, assertions, msecs, **kw):
         self.result.setSectionData('tests', tests)
         self.result.setSectionData('assertions', assertions)
         self.result.setSectionData('msecs', msecs)
         self.result.storeSection()
-        self.context.setState(EndedState(self.context))
+        self.context.setState('ended')
     
-    def fail(self):
-        self.context.setState(FailedState(self.context))
+    def fail(self, **kw):
+        self.context.setState('failed')
     
-    def test(self, name):
+    def test(self, name, **kw):
         self.result.addTest(name)
     
-    def stop(self, assertions, msecs, failures):
+    def stop(self, assertions, msecs, failures, **kw):
         self.result.setTotal('assertions', assertions)
         self.result.setTotal('msecs', msecs)
         self.result.setTotal('failures', failures)
-        self.context.setState(EndedState(self.context))
+        self.context.setState('ended')
 
 class EndedState(State):
-    def start(self, name):
+    def start(self, name, **kw):
         self.result.newSection(name)
-        self.context.setState(StartedState(self.context))
+        self.context.setState('started')
 
 class FailedState(State):    
-    def start(self, name):
+    def start(self, name, **kw):
         self.result.storeSection()
         self.result.newSection(name)
-        self.context.setState(StartedState(self.context))
+        self.current_testcase = None
+        self.context.setState('started')
     
-    def stop(self, assertions, msecs, failures):
+    def stop(self, assertions, msecs, failures, **kw):
         self.result.setTotal('assertions', assertions)
         self.result.setTotal('msecs', msecs)
         self.result.setTotal('failures', failures)
         self.result.storeSection()
-        self.context.setState(EndedState(self.context))
+        self.current_testcase = None
+        self.context.setState('ended')
     
-    def work(self, line):
-        pattern = re.compile('^\d+\)\s+([^\s]+).*line:\s*(.*)')
-        match = re.match(pattern, line)
-        if match:
-            self.result.setTestData(match.group(1), 'passed', False)
-            self.result.appendTestData(match.group(1), 'message', match.group(2).strip()+'\n')
-            self.current_testcase = match.group(1)
-        else:
-            if isinstance(self.current_testcase, str):
-                self.result.appendTestData(self.current_testcase, 'message', line)
+    def handle(self, line, **kw):
+        if isinstance(self.current_testcase, str):
+            self.result.appendTestData(self.current_testcase, 'message', line)
         self.result.appendSectionData('content', line)
     
-    def failResult(self, runs, failures, errors):
+    def failResult(self, runs, failures, errors, **kw):
         self.result.setSectionData('tests', runs)
         self.result.setSectionData('failures', failures)
         self.result.setSectionData('errors', errors)
     
-    def failSuccess(self, assertions, msecs):
+    def failSuccess(self, assertions, msecs, **kw):
         self.result.setSectionData('assertions', assertions)
         self.result.setSectionData('msecs', msecs)
+    
+    def failStartFailure(self, testcase, message, line, **kw):
+        self.result.setTestData(testcase, 'passed', False)
+        self.result.appendTestData(testcase, 'message', message.strip()+'\n')
+        self.current_testcase = testcase
+        self.result.appendSectionData('content', line)
 
 class Parser(object):
     def __init__(self):
         self.result = Result()
+        self.states = {
+                       'started': StartedState(self),
+                       'ended': EndedState(self),
+                       'failed': FailedState(self),
+                       }
         self.patterns = [
                         ( re.compile('^Running (.*)'),
-                          lambda match: self.state.start(name=match.group(1)) ),
+                          lambda line, match: self.state.start(line=line, name=match.group(1)) ),
                         ( re.compile('^OK \((\d+)\D*test\D*(\d+)\D*assertion\D*(\d+)\D*ms.*\)'),
-                          lambda match: self.state.end(tests=match.group(1), assertions=match.group(2), msecs=match.group(3)) ),
+                          lambda line, match: self.state.end(line=line, tests=match.group(1), assertions=match.group(2), msecs=match.group(3)) ),
                         ( re.compile('^--([^-]+)--'),
-                          lambda match: self.state.test(name=match.group(1)) ),
+                          lambda line, match: self.state.test(line=line, name=match.group(1)) ),
                         ( re.compile('^!!!FAILURES!!!'),
-                          lambda match: self.state.fail() ),
+                          lambda line, match: self.state.fail(line=line) ),
                         ( re.compile('^(\d+)\D*assertion\D*(\d+)\D*ms\D*(\d+)\D*failure.*'),
-                          lambda match: self.state.stop(assertions=match.group(1), msecs=match.group(2), failures=match.group(3)) ),
+                          lambda line, match: self.state.stop(line=line, assertions=match.group(1), msecs=match.group(2), failures=match.group(3)) ),
                         ( re.compile('^Run\D*(\d+)\D*Failure\D*(\d+)\D*Error\D*(\d+)'),
-                          lambda match: self.state.failResult(runs=match.group(1), failures=match.group(2), errors=match.group(3)) ),
+                          lambda line, match: self.state.failResult(line=line, runs=match.group(1), failures=match.group(2), errors=match.group(3)) ),
                         ( re.compile('^\((\d+)\D*assertion\D*(\d+)\D*ms.*\)'),
-                          lambda match: self.state.failSuccess(assertions=match.group(1), msecs=match.group(2)) )
+                          lambda line, match: self.state.failSuccess(line=line, assertions=match.group(1), msecs=match.group(2)) ),
+                        ( re.compile('^\d+\)\s+([^\s]+).*line:\s*(.*)'),
+                          lambda line, match: self.state.failStartFailure(line=line, testcase=match.group(1), message=match.group(2)) )
                         ]
     
     def setState(self, state):
-        self.state = state
+        if isinstance(state, str):
+            if self.states.has_key(state):
+                self.state = self.states[state]
+        else:
+            self.state = state
+    
+    def parseLine(self, line):
+        found = False
+        for pattern, event in self.patterns:
+            match = re.match(pattern, line)
+            if match:
+                event(line, match)
+                found = True
+        if not found:
+            self.state.handle(line)
     
     def parse(self, content):
-        self.setState(EndedState(self))
+        self.setState('ended')
         for line in content:
-            found = False
-            for pattern, event in self.patterns:
-                match = re.match(pattern, line)
-                if match:
-                    event(match)
-                    found = True
-            if not found:
-                self.state.work(line)
+            self.parseLine(line)
         return self.result
 
 def dependsOnTestfw(target, registry):
+    import SCons
     if SCons.Util.is_List(target):
         target = target[0]
     plaintarget = registry.getPackageTarget('testfw', 'testfw')['plaintarget']
@@ -181,6 +193,7 @@ def callPostTest(target, registry, packagename, targetname, logfile, **kw):
                     xmlfile.write(result.toXML(packagename+'.'+targetname))
                 
 def generate(env):
+    import RunBuilder
     RunBuilder.registerCallback("PostTest", callPostTest)
 
 def exists(env):
