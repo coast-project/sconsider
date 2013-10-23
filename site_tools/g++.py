acc8c08eb99f36a6a58c015af8e61dc3a69a369e
Marcel Huber <marcel.huber@hsr.ch>
2013-10-23 14:00:40 +0200

    moved -Wundef flag to full warnings section
    boost causes many Wundef warnings which interfere with usual output
    
    Change-Id: I8b46cc60836b860510eadcc55d4a0740c67bceaa

--- site_tools/g++.py
+++ site_tools/g++.py
@@ -21,19 +21,19 @@ import SCons.Tool
 
 compilers = ['g++']
 
-def generate( env ):
+def generate(env):
     """Add Builders and construction variables for g++ to an Environment."""
-    static_obj, shared_obj = SCons.Tool.createObjBuilders( env )
+    static_obj, shared_obj = SCons.Tool.createObjBuilders(env)
 
-    SCons.Tool.Tool( 'c++' )( env )
+    SCons.Tool.Tool('c++')(env)
 
-    if env.get( '_CXXPREPEND_' ):
-        compilers.insert( 0, env.get( '_CXXPREPEND_' ) )
-    env['CXX'] = env.Detect( compilers )
+    if env.get('_CXXPREPEND_'):
+        compilers.insert(0, env.get('_CXXPREPEND_'))
+    env['CXX'] = env.Detect(compilers)
 
     # platform specific settings
     if env['PLATFORM'] == 'aix':
-        env['SHCXXFLAGS'] = SCons.Util.CLVar( '$CXXFLAGS -mminimal-toc' )
+        env['SHCXXFLAGS'] = SCons.Util.CLVar('$CXXFLAGS -mminimal-toc')
         env['STATIC_AND_SHARED_OBJECTS_ARE_THE_SAME'] = 1
         env['SHOBJSUFFIX'] = '$OBJSUFFIX'
     elif env['PLATFORM'] == 'hpux':
@@ -41,49 +41,49 @@ def generate( env ):
     elif env['PLATFORM'] == 'sunos':
         env['SHOBJSUFFIX'] = '.pic.o'
     # determine compiler version
-    gccfss=False
+    gccfss = False
     if env['CXX']:
-        #pipe = SCons.Action._subproc(env, [env['CXX'], '-dumpversion'],
-        pipe = SCons.Action._subproc( env, [env['CXX'], '--version'],
-                                     stdin = 'devnull',
-                                     stderr = 'devnull',
-                                     stdout = subprocess.PIPE )
+        # pipe = SCons.Action._subproc(env, [env['CXX'], '-dumpversion'],
+        pipe = SCons.Action._subproc(env, [env['CXX'], '--version'],
+                                     stdin='devnull',
+                                     stderr='devnull',
+                                     stdout=subprocess.PIPE)
         if pipe.wait() != 0: return
         # -dumpversion was added in GCC 3.0.  As long as we're supporting
         # GCC versions older than that, we should use --version and a
         # regular expression.
-        #line = pipe.stdout.read().strip()
-        #if line:
+        # line = pipe.stdout.read().strip()
+        # if line:
         #    env['CXXVERSION'] = line
         line = pipe.stdout.readline()
-        versionmatch = re.search( r'(\s+)([0-9]+(\.[0-9]+)+)', line )
-        gccfssmatch = re.search( r'(\(gccfss\))', line )
+        versionmatch = re.search(r'(\s+)([0-9]+(\.[0-9]+)+)', line)
+        gccfssmatch = re.search(r'(\(gccfss\))', line)
         if versionmatch:
-            env['CXXVERSION'] = versionmatch.group( 2 )
+            env['CXXVERSION'] = versionmatch.group(2)
         if gccfssmatch:
-            env['CXXFLAVOUR'] = gccfssmatch.group( 1 )
-            gccfss=True
+            env['CXXFLAVOUR'] = gccfssmatch.group(1)
+            gccfss = True
 
-        ## own extension to detect system include paths
+        # # own extension to detect system include paths
         import time
         fName = '.code2Compile.' + str(time.time())
-        tFile = os.path.join( SCons.Script.Dir( '.' ).abspath, fName )
-        outFile = os.path.join( SCons.Script.Dir( '.' ).abspath, fName+'.o' )
+        tFile = os.path.join(SCons.Script.Dir('.').abspath, fName)
+        outFile = os.path.join(SCons.Script.Dir('.').abspath, fName + '.o')
         try:
-            outf = open( tFile, 'w' )
-            outf.write( '#include <cstdlib>\nint main(){return 0;}' )
+            outf = open(tFile, 'w')
+            outf.write('#include <cstdlib>\nint main(){return 0;}')
             outf.close()
         except:
             print "failed to create compiler input file, check folder permissions and retry"
             return
-        pipe = SCons.Action._subproc( env, [env['CXX'], '-v', '-xc++', tFile, '-o', outFile, '-m'+env['ARCHBITS']],
-                                     stdin = 'devnull',
-                                     stderr = subprocess.PIPE,
-                                     stdout = subprocess.PIPE )
+        pipe = SCons.Action._subproc(env, [env['CXX'], '-v', '-xc++', tFile, '-o', outFile, '-m' + env['ARCHBITS']],
+                                     stdin='devnull',
+                                     stderr=subprocess.PIPE,
+                                     stdout=subprocess.PIPE)
         pRet = pipe.wait()
-        os.remove( tFile )
-        try: os.remove( outFile )
-        except: print outFile,"could not be deleted, check compiler output"
+        os.remove(tFile)
+        try: os.remove(outFile)
+        except: print outFile, "could not be deleted, check compiler output"
         if pRet != 0:
             print "---- stdout ----"
             print pipe.stdout.read()
@@ -92,69 +92,69 @@ def generate( env ):
             print "failed to compile object, return code:", pRet
             return
         pout = pipe.stderr.read()
-        reIncl = re.compile( '#include <\.\.\.>.*:$\s((^ .*\s)*)', re.M )
-        match = reIncl.search( pout )
+        reIncl = re.compile('#include <\.\.\.>.*:$\s((^ .*\s)*)', re.M)
+        match = reIncl.search(pout)
         sysincludes = []
         if match:
-            for it in re.finditer( "^ (.*)$", match.group( 1 ), re.M ):
-                sysincludes.append( it.groups()[0] )
+            for it in re.finditer("^ (.*)$", match.group(1), re.M):
+                sysincludes.append(it.groups()[0])
         if sysincludes:
-            env.AppendUnique( SYSINCLUDES = sysincludes )
+            env.AppendUnique(SYSINCLUDES=sysincludes)
 
     platf = env['PLATFORM']
-    env.AppendUnique( CPPDEFINES = ['_POSIX_PTHREAD_SEMANTICS', '_REENTRANT'] )
+    env.AppendUnique(CPPDEFINES=['_POSIX_PTHREAD_SEMANTICS', '_REENTRANT'])
 
-    env.AppendUnique( CCFLAGS = '-m' + env['ARCHBITS'] )
-    if str( platf ) == 'darwin':
+    env.AppendUnique(CCFLAGS='-m' + env['ARCHBITS'])
+    if str(platf) == 'darwin':
         if env['ARCHBITS'] == '32':
-            env.AppendUnique( CCFLAGS = ['-arch', 'i386'] )
+            env.AppendUnique(CCFLAGS=['-arch', 'i386'])
         else:
-            env.AppendUnique( CCFLAGS = ['-arch', 'x86_64'] )
+            env.AppendUnique(CCFLAGS=['-arch', 'x86_64'])
 
-    if not SCons.Script.GetOption( 'no-largefilesupport' ):
-        env.AppendUnique( CPPDEFINES = ['_LARGEFILE64_SOURCE'] )
+    if not SCons.Script.GetOption('no-largefilesupport'):
+        env.AppendUnique(CPPDEFINES=['_LARGEFILE64_SOURCE'])
 
-    buildmode = SCons.Script.GetOption( 'buildcfg' )
+    buildmode = SCons.Script.GetOption('buildcfg')
     if buildmode == 'debug':
-        env.AppendUnique( CXXFLAGS = [ '-ggdb3' if str( platf ) == 'sunos' else '-g'] )
+        env.AppendUnique(CXXFLAGS=[ '-ggdb3' if str(platf) == 'sunos' else '-g'])
     elif buildmode == 'optimized':
-        if str( platf ) == 'sunos':
+        if str(platf) == 'sunos':
             if gccfss:
                 # at least until g++ 4.3.3 (gccfss), there is a bug #100 when using optimization levels above -O1
                 # -> -fast option breaks creation of correct static initialization sequence
-                env.AppendUnique( CXXFLAGS = ['-O1'] )
+                env.AppendUnique(CXXFLAGS=['-O1'])
             else:
-                env.AppendUnique( CXXFLAGS = ['-O3'] )
+                env.AppendUnique(CXXFLAGS=['-O3'])
         else:
-            env.AppendUnique( CXXFLAGS = ['-O3'] )
+            env.AppendUnique(CXXFLAGS=['-O3'])
     elif buildmode == 'profile':
-        env.AppendUnique( CXXFLAGS = ['-fprofile'] )
+        env.AppendUnique(CXXFLAGS=['-fprofile'])
 
-    warnlevel = SCons.Script.GetOption( 'warnlevel' )
+    warnlevel = SCons.Script.GetOption('warnlevel')
     if warnlevel == 'medium' or warnlevel == 'full':
-        env.AppendUnique( CXXFLAGS = [
-            '-Waddress',                #<! Warn about suspicious uses of memory addresses
-            '-Wall',                    #<! Enable most warning messages
+        env.AppendUnique(CXXFLAGS=[
+            '-Waddress',  # <! Warn about suspicious uses of memory addresses
+            '-Wall',  # <! Enable most warning messages
             '-Wdeprecated',
             '-Wendif-labels',
             '-Wno-system-headers',
             '-Woverloaded-virtual',
-            '-Wpointer-arith',          #<! Warn about function pointer arithmetic
+            '-Wpointer-arith',  # <! Warn about function pointer arithmetic
             '-Wreturn-type',
             '-Wshadow',
-            '-Wundef',                  #<! Warn if an undefined macro is used in an #if directive
-        ] )
+        ])
     if warnlevel == 'full':
-        env.AppendUnique( CXXFLAGS = [
-            '-Wcast-qual',              #<! Warn about casts which discard qualifiers
-            '-Wconversion',             #<! Warn for implicit type conversions that may change a value
-            '-Weffc++',                 #<! Warn about violations of Effective C++ style rules
-            '-Wignored-qualifiers',     #<! Warn whenever type qualifiers are ignored.
-            '-Wold-style-cast',         #<! Warn if a C-style cast is used in a program
-            '-Wextra',                  #<! Warn about some extra warning flags that are not enabled by -Wall.
-        ] )
+        env.AppendUnique(CXXFLAGS=[
+            '-Wcast-qual',  # <! Warn about casts which discard qualifiers
+            '-Wconversion',  # <! Warn for implicit type conversions that may change a value
+            '-Weffc++',  # <! Warn about violations of Effective C++ style rules
+            '-Wignored-qualifiers',  # <! Warn whenever type qualifiers are ignored.
+            '-Wold-style-cast',  # <! Warn if a C-style cast is used in a program
+            '-Wextra',  # <! Warn about some extra warning flags that are not enabled by -Wall.
+            '-Wundef',  # <! Warn if an undefined macro is used in an #if directive
+        ])
 
-def exists( env ):
-    if env.get( '_CXXPREPEND_' ):
-        compilers.insert( 0, env.get( '_CXXPREPEND_' ) )
-    return env.Detect( compilers )
+def exists(env):
+    if env.get('_CXXPREPEND_'):
+        compilers.insert(0, env.get('_CXXPREPEND_'))
+    return env.Detect(compilers)
