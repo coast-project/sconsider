0edc21e1b2e4176a13e55c5f04e69edf90cec95b
Marcel Huber <marcelhuberfoo@gmail.com>
2014-07-25 09:14:10 +0200

    setup: adapted setup.py to layout seen in wheel
    (https://bitbucket.org/pypa/wheel/src)
    
    adjusted markdown style in README
    
    documented how to run tests using nosetests
    
    build command should only create a wheel as default
    
    Change-Id: I7fc1582603916467003252dfeff256cfdcd14ae0

--- setup.py
+++ setup.py
@@ -1,52 +1,62 @@
-from setuptools import setup, find_packages
+import os.path
+import codecs
+import re
 
-setup(
-    name="SConsider",
-    version="1.5.0",
-    packages=find_packages(
-        exclude=[
-            '*.maintenance',
-            'tests']),
-    install_requires=[
-        'scons >=1.3, <=2.3.0',
-        'pyaml',
-        'pyopenssl',
-        'lepl'],
-    package_data={
-        '': [
-            'license.txt',
-            '*.yaml',
-            '3rdparty/*/*.sconsider',
-            'site_tools/*.sh'],
-    },
-    dependency_links=[],
-    include_package_data=True,
-    setup_requires = ['setuptools-git >= 1.0', 'gitegginfo'],
-    exclude_package_data = {'': ['.gitignore', '.gitreview', '.project', '.pydevproject'],},
-    test_suite='tests',
-    tests_require=['nose','mockito'],
-    author="Marcel Huber",
-    author_email="marcel.huber@hsr.ch",
-    license="BSD",
-    description="scons build system extension",
-    long_description="""\
-The main goal/feature of this SCons extension is to provide a recursive target
-detection and dependency handling mechanism.
-""",
-    url="https://redmine.coast-project.org/projects/sconsider",
-    # classifier list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-                 "Development Status :: 4 - Beta",
-#                 "Development Status :: 5 - Production/Stable",
-                 "Environment :: Console",
-                 "Intended Audience :: Developers",
-                 "License :: OSI Approved :: BSD License",
-                 "Natural Language :: English",
-                 "Operating System :: MacOS :: MacOS X",
-                 "Operating System :: Microsoft :: Windows",
-                 "Operating System :: POSIX :: Linux",
-                 "Operating System :: POSIX :: SunOS/Solaris",
-                 "Programming Language :: Python",
-                 "Programming Language :: Python :: 2.7",
-                 "Topic :: Software Development :: Build Tools"],
-)
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = codecs.open(os.path.join(here, 'README.txt'), encoding='utf8').read()
+CHANGES = codecs.open(
+    os.path.join(
+        here,
+        'CHANGES.txt'),
+    encoding='utf8').read()
+
+with codecs.open(os.path.join(os.path.dirname(__file__), 'SConsider', 'version.py'),
+                 encoding='utf8') as version_file:
+    metadata = dict(
+        re.findall(
+            r"""__([a-z]+)__ = "([^"]+)""",
+            version_file.read()))
+
+setup(name="SConsider",
+      version=metadata['version'],
+      description="scons build system extension",
+      long_description=README + '\n\n' + CHANGES,
+      # classifier list:
+      # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          # "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: BSD License",
+          "Natural Language :: English",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows",
+          "Operating System :: POSIX :: Linux",
+          "Operating System :: POSIX :: SunOS/Solaris",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Build Tools"],
+      author="Marcel Huber",
+      author_email="marcel.huber@hsr.ch",
+      url="https://redmine.coast-project.org/projects/sconsider",
+      keywords=['sconsider', 'scons', 'build'],
+      license="BSD",
+      packages=[
+          'SConsider',
+          'SConsider.site_tools',
+          'SConsider.xmlbuilder'],
+      install_requires=[
+          'scons >=1.3, <=2.3.0',
+          'pyaml',
+          'pyopenssl',
+          'lepl'],
+      setup_requires=['gitegginfo', 'flake8'],
+      test_suite='tests',
+      tests_require=['nose', 'mockito'],
+      include_package_data=True,
+      zip_safe=False,
+      )
