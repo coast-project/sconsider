830eeccbe6b2d68c46b9f7023282578b539ce9fa
Marcel Huber <marcelhuberfoo@gmail.com>
2016-07-19 13:30:21 +0200

    ensure PackageRegistry is initialized for tests
    Change-Id: I7edadcfe38c5013beefcfd9fcb1bb094e6097327

--- tests/test_PackageRegistry.py
+++ tests/test_PackageRegistry.py
@@ -8,35 +8,43 @@
 # library/application in the file license.txt.
 # -------------------------------------------------------------------------
 
+import pytest
 from SConsider.PackageRegistry import PackageRegistry
 
 
-class TestSplitTargetname(object):
-    def test_SplitTargetname(self):
-        pkgname, tgtname = PackageRegistry.splitFulltargetname('package.target')
-        assert 'package' == pkgname
-        assert 'target' == tgtname
-
-    def test_SplitTargetnameNoTarget(self):
-        pkgname, tgtname = PackageRegistry.splitFulltargetname('package')
-        assert 'package' == pkgname
-        assert None is tgtname
-
-    def test_SplitTargetnameDefault(self):
-        pkgname, tgtname = PackageRegistry.splitFulltargetname('package', True)
-        assert 'package' == pkgname
-        assert 'package' == tgtname
+@pytest.fixture(scope="module")
+def registry():
+    return PackageRegistry(None)
 
 
-class TestGenerateFulltargetname(object):
-    def test_GenerateFulltargetname(self):
-        ftn = PackageRegistry.createFulltargetname('package', 'target')
-        assert 'package.target' == ftn
+def test_SplitTargetname(registry):
+    pkgname, tgtname = PackageRegistry.splitFulltargetname('package.target')
+    assert 'package' == pkgname
+    assert 'target' == tgtname
 
-    def test_GenerateFulltargetnameNoTarget(self):
-        ftn = PackageRegistry.createFulltargetname('package')
-        assert 'package' == ftn
 
-    def test_GenerateFulltargetnameDefault(self):
-        ftn = PackageRegistry.createFulltargetname('package', default=True)
-        assert 'package.package' == ftn
+def test_SplitTargetnameNoTarget(registry):
+    pkgname, tgtname = PackageRegistry.splitFulltargetname('package')
+    assert 'package' == pkgname
+    assert None is tgtname
+
+
+def test_SplitTargetnameDefault(registry):
+    pkgname, tgtname = PackageRegistry.splitFulltargetname('package', True)
+    assert 'package' == pkgname
+    assert 'package' == tgtname
+
+
+def test_GenerateFulltargetname(registry):
+    ftn = PackageRegistry.createFulltargetname('package', 'target')
+    assert 'package.target' == ftn
+
+
+def test_GenerateFulltargetnameNoTarget(registry):
+    ftn = PackageRegistry.createFulltargetname('package')
+    assert 'package' == ftn
+
+
+def test_GenerateFulltargetnameDefault(registry):
+    ftn = PackageRegistry.createFulltargetname('package', default=True)
+    assert 'package.package' == ftn
