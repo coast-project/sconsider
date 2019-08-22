3f200ff327a30c0baa579d3fc0781eef22180174
Marcel Huber <marcel.huber@hsr.ch>
2019-08-22 16:09:28 +0200

    update of changelog and change of mime type

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     description="scons build system extension",
     long_description=read_file(os.path.join(BASEDIR, 'README.adoc')),
-    long_description_content_type="text/plain",
+    long_description_content_type="text/asciidoc",
     # classifier list:
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
